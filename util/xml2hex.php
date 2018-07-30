6559525f3d5be437b98b822539deeed7391a9155
Coder96 <none@none.com>
2018-07-30 10:54:01 -0500

    Added command line options

--- util/xml2hex.php
+++ util/xml2hex.php
@@ -25,15 +25,18 @@
 		'8' => '08080808',
 		'9' => '09090909'
 	);
-
-	$fileToload = $argv[1] ;
+	$GLOBALS['myapp']['debug'] 					= false;
+	$GLOBALS['myapp']['inFileName']     = '';
+	$GLOBALS['myapp']['outFileName']    = '';
 	
 	
-	$xml = simplexml_load_file($fileToload);
+	cliParms();
+	
+	$xml = simplexml_load_file($GLOBALS['myapp']['inFileName']);
 	if ($xml === false) {
-		echo "Failed loading XML: ";
+		writeErr("Failed loading XML: ");
 		foreach(libxml_get_errors() as $error) {
-			echo "\n", $error->message;
+			writeErr($error->message);
 		}
 	} else {
 		$hexBigSring = '';
@@ -48,54 +51,165 @@
 				$controlCodes['recordDelimiter'];
 		}
 		
-		writeToFile($fileToload, $hexBigSring);
+		writeToFile($GLOBALS['myapp']['outFileName'], $hexBigSring);
 	}
-	/******************************************************************************************
-	*
-	*	This will build the build the hex string and conver it to little endian.
-	*
-	*
-	*
-	*/
-	function buildPictureHexString($csvString){
-		//$h1 = '$78_56_34_12';
-		//       012345678901
-		$hexString = '';
-		$list = str_getcsv($csvString);
-		foreach($list as $item){
-			$h1 = trim($item);
-			$hexString = $hexString . substr($h1,10,2) . substr($h1,7,2) . substr($h1,4,2) . substr($h1,1,2);
+//*********************************************************************
+//
+// This will build the build the hex string and conver it to little endian.
+//
+//
+//
+//*********************************************************************
+function buildPictureHexString($csvString){
+	//$h1 = '$78_56_34_12';
+	//       012345678901
+	$hexString = '';
+	$list = str_getcsv($csvString);
+	foreach($list as $item){
+		$h1 = trim($item);
+		$hexString = $hexString . substr($h1,10,2) . substr($h1,7,2) . substr($h1,4,2) . substr($h1,1,2);
+	}
+	return($hexString);
+}
+//*********************************************************************
+//
+//	This will build the build the hex string and conver it to little endian.
+//
+//
+//
+//*********************************************************************
+function buildPauseHexString($pauseValue){
+	
+	$hexString = dechex(intval($pauseValue));
+	$hexString = sprintf("%08s",$hexString);
+	// 12345678
+	// 01234567
+	$hexString = substr($hexString,6,2) . substr($hexString,4,2) . substr($hexString,2,2) . substr($hexString,0,2);
+	return $hexString;
+}
+//*********************************************************************
+//
+//
+//
+//*********************************************************************
+function writeToFile($iFileName, $hexString){
+	$hex = hex2bin($hexString);
+	file_put_contents($iFileName, $hex);
+}
+//*********************************************************************
+//
+//
+//
+//*********************************************************************
+
+function cliParms(){
+
+	$error = false;
+	$errorMessage = '';
+	
+	$shortopts  = "h";
+
+	$longopts  = array(
+		'help',
+		'debugOn',
+		'inFileName:',
+		'outFileName::',
+           
+		);
+	$options = getopt($shortopts, $longopts);	
+	
+	if(isset($options['debugOn'])){
+		debugSet(true);
+	} else {
+		debugSet(false);
+	}
+	
+	if(isset($options['inFileName'])){
+		if(!inFileName($options['inFileName'])){
+			$error = true;
+			writeErr('--inFileName file not found:'.$options['inFileName']);	
 		}
-		return($hexString);
+	} else {
+		$GLOBALS['myapp']['inFileName'] = 'php://STDIN';
 	}
-	/******************************************************************************************
-	*
-	*	This will build the build the hex string and conver it to little endian.
-	*
-	*
-	*
-	*/
-	function buildPauseHexString($pauseValue){
+
+	if(isset($options['outFileName'])){
+		if($options['outFileName'] == 'STDOUT'){
+			outFileName('php://STDOUT');
+		} else {
+			outFileName($options['outFileName']);
+		}
+	} else {
+		outFileName('php://STDOUT');
+	}
+	
+	if(isset($options['h']) or isset($options['help']) or $error or isset($options['scriptFileHelp'])){
+
+		if($error){
+			writeErr("Error exiting.");
+		}
 		
-		$hexString = dechex(intval($pauseValue));
-		$hexString = sprintf("%08s",$hexString);
-		// 12345678
-		// 01234567
-		$hexString = substr($hexString,6,2) . substr($hexString,4,2) . substr($hexString,2,2) . substr($hexString,0,2);
-		return $hexString;
+		if(isset($options['h']) or isset($options['help']) ){
+			writeErr('
+
+ Options:
+
+   --inFileName         Default STDIN.
+   --outFileName        Default STDOUT.
+
+   $'.__FILE__.' --inFileName=test.xml --outFileName=test.hex
+
+');
+		}
+
+		exit();
+	
 	}
-	/******************************************************************************************
-	*
-	*	
-	*
-	*
-	*
-	*/
-	function writeToFile($fileName, $hexString){
-		$hex = hex2bin($hexString);
-		$fileName = basename($fileName, ".xml");
-		file_put_contents("tmp/$fileName.hex", $hex);
-	}
+}	
+//*********************************************************************
+//
+//
+//
+//*********************************************************************
+function writeErr($line){
+	fwrite(STDERR,"$line\n");
+}
+//*********************************************************************
+//
+//
+//
+//*********************************************************************
+function debugSet($set){
+
+		$GLOBALS['myapp']['debug'] = $set;
+}	
+//*********************************************************************
+//
+//
+//
+//*********************************************************************
+function inFileName($iFile){
+
+		if(is_file($iFile)){
+			$GLOBALS['myapp']['inFileName'] = $iFile;
+		} else {
+			writeErr('inFileName file not found:'.$iFile);	
+			return false;
+		}
+	return true;
+}
+//*********************************************************************
+//
+//
+//
+//*********************************************************************
+function outFileName($iFile){
+	$GLOBALS['myapp']['outFileName'] = $iFile;
+}	
+	
+	
+	
+	
 	
 	
 	
